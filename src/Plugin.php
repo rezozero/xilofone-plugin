c9c9c5525e2191b426794dad9e4f1182a59f2059
Ambroise Maupate <ambroise@rezo-zero.com>
2023-09-19 18:32:44 +0200

    chore: whitespaces

--- src/Plugin.php
+++ src/Plugin.php
@@ -17,17 +17,14 @@ final class Plugin implements PluginInterface, Capable, EventSubscriberInterface
 {
     public function activate(Composer $composer, IOInterface $io): void
     {
-
     }
 
     public function deactivate(Composer $composer, IOInterface $io): void
     {
-
     }
 
     public function uninstall(Composer $composer, IOInterface $io): void
     {
-
     }
 
     public function getCapabilities(): array
